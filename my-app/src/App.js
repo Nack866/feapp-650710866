575ab7358d9bf77d458498caad5ff6c95a868195
Nack866 <115211604+Nack866@users.noreply.github.com>
2026-08-20 05:06:48 +0000

    lab ProfileCard

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,24 +4,48 @@ import MovieList from './components/MovieList';
 import Footer from './components/Footer';
 import Greeting from './components/Greeting';
 import Card from './components/Card';
+import ProfileCard from './components/ProfileCard';
 
-const students = [
-  { id: 1, name: 'ฝน',   year: 3 },
-  { id: 2, name: 'เต้ย', year: 2 },
-  { id: 3, name: 'มายด์', year: 4 },
+
+const members = [
+  {
+    id: 1,
+    name: 'สมหญิง ใจดี',
+    nickname: 'ฝน',
+    major: 'เทคโนโลยีสารสนเทศ',
+    favorites: ['ชาเขียว', 'แมว']
+  },
+  {
+    id: 2,
+    name: 'สมชาย ใจดี',
+    nickname: 'บอล',
+    major: 'วิทยาการคอมพิวเตอร์',
+    favorites: ['กาแฟ', 'ฟุตบอล']
+  },
+  {
+    id: 3,
+    name: 'นภัสสร แสนดี',
+    nickname: 'นุ่น',
+    major: 'การออกแบบดิจิทัล',
+    favorites: ['ชานม', 'สุนัข']
+  }
 ];
 
 function App() {
   return (
-    <div>
-      <Card title="ประกาศ">
-      <p>สัปดาห์หน้าเรียนเรื่อง <b>State & Forms</b></p>
-      <p>อย่าลืม push การบ้านขึ้น GitHub!</p>
-      </Card>
-      <h1>รายชื่อนักศึกษา</h1>
-      {students.map((s) => (
-        <Greeting key={s.id} name={s.name} year={s.year} />
-      ))}
+    <div className="container">
+      <h1>สมาชิกกลุ่มของเรา</h1>
+      <div className="card-row">
+        {members.map((m) => (
+          <ProfileCard
+            key={m.id}
+            name={m.name}
+            nickname={m.nickname}
+            major={m.major}
+            favorites={m.favorites}
+          />
+        ))}
+      </div>
     </div>
   );
 }
